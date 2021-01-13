a191e0a9e35beac1b2563cf4beb6dd93b26fb6c8
Ruut <ruut.sofia@gmail.com>
2021-01-13 12:30:53 +0200

    added allow origin header

--- nameController.js
+++ nameController.js
@@ -2,6 +2,14 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authentication");
+    next();
+});
 
 const readNamesFromFile = () => {
     const file = fs.readFileSync("./names.json",
